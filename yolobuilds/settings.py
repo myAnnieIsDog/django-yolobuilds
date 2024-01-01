91620190593934066ad4c1f79ccc4a4350134278
myAnnieIsDog <146695251+myAnnieIsDog@users.noreply.github.com>
2024-01-01 15:46:49 -0800

    Collect Static Files

--- yolobuilds/settings.py
+++ yolobuilds/settings.py
@@ -151,6 +151,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = "static/"
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
